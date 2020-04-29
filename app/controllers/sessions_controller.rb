8f127729a0cfdad3acc3d14fcfb7df3d935a9823
EC2 Default User <ec2-user@ip-172-31-87-184.ec2.internal>
2020-04-29 00:51:41 +0000

    changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,30 @@
 class SessionsController < ApplicationController
     def create
-        # auth_hash = request.env['omniauth.auth']
+        auth = request.env['omniauth.auth']
+        session[:omniauth] = auth.except('extra')
+        user = User.sign_in_from_omniauth(auth)
+        
+        session[:user_id] = user.id unless auth["credentials"].blank?
+        redirect_to root_url, notice: "SIGNED IN"
+        
+        #other article
+        # user = User.find_by(twitter_handle: auth[:info][:nickname])
+        # user ||= User.create!(
+        # full_name: auth[:info][:name],
+        # twitter_handle: auth[:info][:nickname],
+        # location: auth[:info][:location]
+        # )
+        # session[:user_id] = user.id
+        # redirect_to movie_path
+        
+    end
+    def destroy
+        session[:user_id] = nil
+        session[:omniauth] = nil
+        redirect_to root_url, notice:"SIGNED OUT"
+    end 
+end
+ # auth_hash = request.env['omniauth.auth']
         # user = User.find_by(twitter_handle: auth_hash[:info][:nickname])
         # user ||= User.create!(
         # full_name: auth_hash[:info][:name],
@@ -9,15 +33,3 @@ class SessionsController < ApplicationController
         # )
         # session[:user_id] = user.id
         # redirect_to dashboard_path
-        auth = request.env['omniauth.auth']
-        session[:omniauth] = auth.except('extra')
-        user = User.sign_in_from_omniauth(auth)
-        session[:user_id] = user.id
-        redirect_to root_url, notice: "SIGNED IN"
-    end
-    def destroy
-        session[:user_id] = nil
-        session[:omniauth] = nil
-        redirect_to root_url, notice:"SIGNED OUT"
-    end 
-end
